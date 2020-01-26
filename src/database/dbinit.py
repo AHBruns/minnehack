0980467906235550c99f9ede42b5430619d7d18a
Bridget Gururaj <gnana005@umn.edu>
2020-01-25 23:56:07 -0600

    database model

--- src/database/dbinit.py
+++ src/database/dbinit.py
@@ -1,16 +1,24 @@
 from pymongo import MongoClient
 import datetime
-import func.py
+from bin.py import bin
 
-client = MongoClient('mongodb+srv://moon-emoji:<password>@cluster0-wyzvi.mongodb.net/test?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://moon-emoji:moonemojiftw123@cluster0-wyzvi.mongodb.net/test?retryWrites=true&w=majority')
 
 db = client['moon-emoji']
 
 ## https://api.mongodb.com/python/current/tutorial.html ##
 
 #--creating a document--#
-def docCreate(breaks24hours, breaks7days, allBreaks, location, breaks)
-    can_data = {"Breaks in 24 Hours": breaks24hours, 
-                "Breaks in 7 Days": breaks7days,
-                "Total Breaks": allBreaks,
-                "Location": 
+can_data = {
+            "Breaks in 24 Hours": 23, 
+            "Breaks in 7 Days": 147,
+            "Total Breaks": 23460,
+            "Location": [44.9731, 93.2354],
+            "Breaks": [(1244534, 300), (1244534, 256)]
+            }
+
+can_datas = db.can_datas
+can_data_id = can_datas.insert_one(can_data).inserted_id
+
+
+
