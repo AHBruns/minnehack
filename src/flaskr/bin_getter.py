18b4963a099522ef2cace9435108a4ef0760d42d
iommk1 <iomma001@umn.edu>
2020-01-26 11:37:48 -0600

    updated can_datas field

--- src/flaskr/bin_getter.py
+++ src/flaskr/bin_getter.py
@@ -1,35 +1,31 @@
 from bson.objectid import ObjectId
 from flask import Flask
-from pymongo import MongoClient
 from flask_cors import CORS, cross_origin
-
+from pymongo import MongoClient
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
 client = MongoClient('mongodb+srv://moon-emoji:moonemojiftw123@cluster0-wyzvi.mongodb.net/test?retryWrites=true&w=majority')
 db = client["moon-emoji"]
-#id = "5e2d14eaa1a3b2634647c92a"
 
 @app.route('/')
 def index():
     return "index"
 
+@app.route('/all')
+def get_all_cans():
+    return None
+
 @app.route('/<id>')
-def get_bin(id):
+def get_can(id):
     can = db.can_datas.find_one({"_id": ObjectId(id)})
     can = {
-        "Breaks in 24 Hours" : can["Breaks in 24 Hours"],
+        "Total Breaks" : can["Total Breaks"],
         "Breaks" : can["Breaks"],
-        "Breaks in 7 Days" : can["Breaks in 7 Days"],
-        "Location" : can["Location"],
+        "Full" : can["Full"],
         "Empties" : can["Empties"],
-        "Total Breaks" : can["Empties"]
+        "Location" : can["Location"]
     }
-    print(can['Breaks in 24 Hours'])
     return can
-
-if __name__=='__main__':
-    get_bin()
-
-#lat long breaks (time and time of fall) tot breaks
