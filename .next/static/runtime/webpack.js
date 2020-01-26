e80f632f74500e7bdcddb4447b99381d79f916f7
Bridget Gururaj <gnana005@umn.edu>
2020-01-25 20:27:55 -0600

    new changes

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f67cef7aac3bd43d33a8";
+/******/ 	var hotCurrentHash = "331af319c966a3a66d2c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
